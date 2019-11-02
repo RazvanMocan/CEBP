bbfce066bca614769475863ecdd76e8ac084945e
AndreiBulzan <45401136+AndreiBulzan@users.noreply.github.com>
2019-11-02 15:03:52 +0200

    Add files via upload
    Ay c'mon

---
+++ com/stock/clients/Seller.java
@@ -0,0 +1,24 @@
+package com.stock.clients;
+
+import com.stock.transactions.Transaction;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Seller extends Client {
+    public  Seller(Socket socket, BufferedReader in) throws IOException {
+        super(socket, in);
+        this.type = "is selling";
+    }
+
+    @Override
+    protected void doTransaction(Transaction sell) {
+        broker.addSellOffer(sell);
+        boolean searching = true;
+        Transaction buy;
+
+        while ( (buy = broker.getBuyOffer(sell.getPrice())) != null && searching )
+            searching = isSearching(sell, buy);
+    }
+}
